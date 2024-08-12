cf7be0e3d690bdd69d27995af5521a456e7faf68
Faiz Abdul Ghoni <69445530+tekotechkotech@users.noreply.github.com>
2024-08-12 21:20:31 +0700

    new menu

--- resources/views/auth-menu.blade.php
+++ resources/views/auth-menu.blade.php
@@ -27,7 +27,7 @@
       @include('layouts.logos')
       <div class="row">
         <div class="col-lg-6 col-md-12 col-sm-12">
-          <a href="manage.{{ $instansi->web }}" class="menu w-100">
+          <a href="https://manage.{{ $instansi->web }}" class="menu w-100">
             <div class="card mb-0">
               <div class="row g-0">
                 <div class="col-md-2 text-center ">
@@ -45,7 +45,7 @@
           </a>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12">
-          <a href="course.{{ $instansi->web }}" class="menu w-100">
+          <a href="https://course.{{ $instansi->web }}" class="menu w-100">
             <div class="card mb-0">
               <div class="row g-0">
                 <div class="col-md-2 text-center ">
@@ -63,7 +63,7 @@
           </a>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12">
-          <a href="test.{{ $instansi->web }}" class="menu w-100">
+          <a href="https://test.{{ $instansi->web }}" class="menu w-100">
             <div class="card mb-0">
               <div class="row g-0">
                 <div class="col-md-2 text-center ">
@@ -83,7 +83,7 @@
           </a>
         </div>
         <div class="col-lg-6 col-md-12 col-sm-12">
-          <a href="billing.{{ $instansi->web }}" class="menu w-100">
+          <a href="https://billing.{{ $instansi->web }}" class="menu w-100">
             <div class="card mb-0">
               <div class="row g-0">
                 <div class="col-md-2 text-center ">
